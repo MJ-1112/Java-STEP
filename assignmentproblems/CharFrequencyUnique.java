7b581c7c0d06e07f63aea06b7bf74abed5d1e682
MJ-1112 <m22327803@gmail.com>
2025-08-27 10:32:03 +0530

    Week1 Assignmetprobems added

---
+++ assignmentproblems/CharFrequencyUnique.java
@@ -0,0 +1,30 @@
+package week1.assignmentproblems;
+
+import java.util.Scanner;
+
+public class CharFrequencyUnique {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        int[] freq = new int[256];  // frequency array for ASCII chars
+
+        // Count frequency of each character
+        for (int i = 0; i < text.length(); i++) {
+            freq[text.charAt(i)]++;
+        }
+
+        System.out.println("Character frequencies:");
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (freq[ch] > 0) {
+                System.out.println(ch + " : " + freq[ch]);
+                freq[ch] = 0;  // reset to avoid duplicate printing
+            }
+        }
+
+        sc.close();
+    }
+}
+
